ab096a2bd09390cfa722cd81e7ecf349e37176b8
Ambrosie Cazimira <a.cazimira@gmail.com>
2022-10-31 01:56:51 +0100

    initialisation projet

--- src/App.js
+++ src/App.js
@@ -2,12 +2,10 @@ import { BrowserRouter, Route, Routes } from "react-router-dom";
 import About from "./pages/About";
 import BooksList from "./pages/BooksList";
 import Home from "./pages/Home";
-import TopNav from "./components/navigation/TopNav";
 
 function App() {
   return (
     <BrowserRouter>
-      <TopNav />
       <Routes>
         <Route path={"/"} element={<Home />} />
         <Route path={"/bookslist"} element={<BooksList />} />
---
+++ src/pages/Home.jsx
@@ -0,0 +1,15 @@
+import React from "react";
+
+export default function Home() {
+  return (
+    <div>
+      <>
+        <div>
+          <h1>Les derneiers sortis par catégorie</h1>
+          <h2>Les mieux notés</h2>
+          <h2>Les plus populaires</h2>
+        </div>
+      </>
+    </div>
+  );
+}
