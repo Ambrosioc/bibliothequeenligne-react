10cc3921c606350a896e567515d330e880ece5de
Ambrosie Cazimira <a.cazimira@gmail.com>
2022-10-31 02:01:38 +0100

    add NotFound Page

--- src/App.js
+++ src/App.js
@@ -2,6 +2,7 @@ import { BrowserRouter, Route, Routes } from "react-router-dom";
 import About from "./pages/About";
 import BooksList from "./pages/BooksList";
 import Home from "./pages/Home";
+import NotFound from "./pages/NotFound";
 
 function App() {
   return (
@@ -10,6 +11,7 @@ function App() {
         <Route path={"/"} element={<Home />} />
         <Route path={"/bookslist"} element={<BooksList />} />
         <Route path={"/about"} element={<About />} />
+        <Route path={"*"} element={<NotFound />} />
       </Routes>
     </BrowserRouter>
   );
