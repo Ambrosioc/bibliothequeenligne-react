57c5e28ce5d404ec567b5ff461935f2b276c7706
Ambrosie Cazimira <a.cazimira@gmail.com>
2022-10-31 02:56:45 +0100

    HomePage create and NotFoundPage

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,15 +1,30 @@
 import React from "react";
+import styled from "styled-components";
+import TestCard from "../components/cards/TestCard";
+
+const HomeContainer = styled.div`
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  justify-content: center;
+`;
 
 export default function Home() {
   return (
-    <div>
-      <>
-        <div>
-          <h1>Les derneiers sortis par catégorie</h1>
-          <h2>Les mieux notés</h2>
-          <h2>Les plus populaires</h2>
-        </div>
-      </>
-    </div>
+    <HomeContainer>
+      <h1>Bienvenue sur la bibliothèque en ligne</h1>
+      <div>
+        <h2>Les derneiers sortis par catégorie</h2>
+        <TestCard />
+      </div>
+      <div>
+        <h2>Les mieux notés</h2>
+        <TestCard />
+      </div>
+      <div>
+        <h2>Les plus populaires</h2>
+        <TestCard />
+      </div>
+    </HomeContainer>
   );
 }
