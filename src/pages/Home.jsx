aa01eb65bb3f5d12451d50cf9c6e7535cbb5481c
Ambrosie Cazimira <a.cazimira@gmail.com>
2022-11-02 16:05:08 +0100

    deleted useFetch

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,7 +1,6 @@
 import React from "react";
 import styled from "styled-components";
 import LastBooks from "../components/cards/LastBooks";
-import { useFetch } from "../utils/hooks";
 import { Loader } from "../utils/styles/Loading";
 
 const HomeContainer = styled.div`
@@ -12,26 +11,20 @@ const HomeContainer = styled.div`
 `;
 
 export default function Home() {
-  const { loading } = useFetch("");
-
   return (
     <HomeContainer>
       <h1>Bienvenue sur la bibliothèque en ligne</h1>
       <div>
         <h2>Les derneiers sortis par catégorie</h2>
-        {loading ? <Loader /> : <LastBooks />}
+        <LastBooks />
       </div>
       <div>
         <h2>Les mieux notés</h2>
-        {loading ? <Loader /> : <div>Pas de livre noté pour le moment !</div>}
+        <Loader />
       </div>
       <div>
         <h2>Les plus populaires</h2>
-        {loading ? (
-          <Loader />
-        ) : (
-          <div>Pas de livre populaire pour le moment !</div>
-        )}
+        <Loader />
       </div>
     </HomeContainer>
   );
