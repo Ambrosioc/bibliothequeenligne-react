a38081c2e91f8de96cd3fc9b9352ee5f86f2c0e5
Ambrosie Cazimira <a.cazimira@gmail.com>
2022-12-27 11:41:49 +0100

    add condition for view page

--- src/pages/BookShow.jsx
+++ src/pages/BookShow.jsx
@@ -1,7 +1,17 @@
 import React from "react";
 import BookInfo from "../container/BookInfo";
+import { Container, StyledLink } from "../utils/styles/GlobalStyle";
 
 export default function BookShow() {
+  const token = localStorage.getItem("token");
+  if (!token) {
+    return (
+      <Container>
+        <h1>Vous devez être connecté pour accéder à cette page</h1>
+        <StyledLink to="/sign-in">Se connecter</StyledLink>
+      </Container>
+    );
+  }
   return (
     <div>
       <BookInfo />
