78cbaf4882f9a3f213bafa6cc431bda1bc247624
Ambrosie Cazimira <a.cazimira@gmail.com>
2023-01-02 07:28:33 +0100

    fix bug react-dom.development.js:22839 Uncaught n

--- src/App.js
+++ src/App.js
@@ -1,5 +1,4 @@
-import { useEffect, useState } from "react";
-import { Route, Routes, useNavigate, useParams } from "react-router-dom";
+import { Route, Routes, useNavigate } from "react-router-dom";
 import SignIn from "./components/auth/SignIn";
 import SignUp from "./components/auth/SignUp";
 import Navbar from "./components/layout/header/Navbar";
@@ -9,37 +8,17 @@ import Home from "./pages/Home";
 import NotFound from "./pages/NotFound";
 import AdminBackOffice from "./pages/private/AdminBackOffice";
 import UserBackOffice from "./pages/private/UserBackOffice";
-import jwtDecode from "jwt-decode";
 import BOUsersList from "./container/BOUsersList";
 import AddBook from "./container/AddBook";
 import UpdateBook from "./container/UpdateBook";
 import BookShow from "./pages/BookShow";
 
 function App() {
-  const [user, setUser] = useState(null);
-
-  const connectUser = () => {
-    const token = localStorage.getItem("token");
-
-    if (token) {
-      const decodedToken = jwtDecode(token);
-      if (decodedToken.exp * 1000 < new Date().getTime()) {
-        localStorage.removeItem("token");
-      } else {
-        setUser(decodedToken);
-      }
-    }
-  };
-
-  useEffect(() => {
-    connectUser();
-  }, []);
-
   const navigate = useNavigate();
 
   return (
     <>
-      <Navbar user={user} />
+      <Navbar />
       <Routes>
         <Route path={"/"} element={<Home />} />
         <Route path={"/admin"} element={<AdminBackOffice />} />
@@ -55,7 +34,6 @@ function App() {
           element={
             <SignIn
               handleLoginSuccess={() => {
-                connectUser();
                 navigate("/");
               }}
             />
